a16d0ceb76176298c6b0b1b44c73ae34b1061c56
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-06-29 02:50:35 +0300

    feat(app): add store to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "./store/hooks/hooks";
+import { getCurrency } from "./store/selectors/currencySelectors";
+import { featchCurrency } from "./store/slices/currencySlice";
 
 function App() {
+  const currencies = useAppSelector(getCurrency);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(featchCurrency());
+  }, [dispatch]);
+
+  console.log(currencies);
   return <div className="App"></div>;
 }
 
