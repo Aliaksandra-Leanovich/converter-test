5bdf71828641d01b3874e8681868fb42d749a93c
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-06-30 00:38:36 +0300

    feat(form): changes in form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import React, { useEffect } from "react";
 import styled from "styled-components";
 import { ContainerCalculateForm } from "./components/ContainerCalculateFrom/ContainerCalculateForm";
-import { StyledContainer } from "./components/ContainerCalculateFrom/style";
-import { useAppDispatch, useAppSelector } from "./store/hooks/hooks";
-import {
-  getCurrency,
-  getCurrencyStatus,
-} from "./store/selectors/currencySelectors";
-import { featchCurrency } from "./store/slices/currencySlice";
+import { useAppSelector } from "./store/hooks/hooks";
+import { getCurrencyStatus } from "./store/selectors/currencySelectors";
 
 function App() {
-  const currencies = useAppSelector(getCurrency);
-  const dispatch = useAppDispatch();
   const status = useAppSelector(getCurrencyStatus);
-
-  useEffect(() => {
-    dispatch(featchCurrency());
-  }, [dispatch]);
 
   if (status === "loading") {
     return <p>loading</p>;
@@ -24,11 +12,6 @@ function App() {
   if (status === "error") {
     return <p>error</p>;
   }
-  console.log(currencies);
-
-  // for (const [key, value] of Object.entries(currencies.rates)) {
-  //   console.log(`${key}: ${value}`);
-  // }
 
   return (
     <StyledApp className="App">
--- src/components/CalculateForm/CalculateForm.tsx
+++ src/components/CalculateForm/CalculateForm.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/hooks/hooks";
+import { getCurrency } from "../../store/selectors/currencySelectors";
+import { featchCurrency } from "../../store/slices/currencySlice";
 import { CalculateInput } from "../CalculateInput/CalculateInput";
 import { StyledForm } from "./style";
 
 export const CalculateForm = () => {
+  const currencies = useAppSelector(getCurrency);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(featchCurrency());
+  }, [dispatch]);
+
+  console.log(currencies);
+
   return (
     <StyledForm>
       <CalculateInput />
