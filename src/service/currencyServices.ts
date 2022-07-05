85af06fb4ed92de37a45a3afbd6aee958a4a52c8
«aleksandrics» <aliaksandraleanovich@gmail.com>
2022-07-06 01:21:15 +0300

    feat(service): change in getting data

--- src/service/currencyServices.ts
+++ src/service/currencyServices.ts
@@ -1,19 +1,32 @@
 import axios from "axios";
-import { from, Observable } from "rxjs";
+import { from, Observable, of } from "rxjs";
 import { map } from "rxjs/operators";
 import { ICurrency, Response } from "../types";
 
+const API_URL = "https://api.exchangerate.host";
+const DIFFERENCE_THRESHOLD_SEC = 5 * 60 * 1000; // 5 minutes threshold
+
 class CurrencyService {
-  private readonly API_URL = "https://api.exchangerate.host";
+  private lastRequestTimestamp = 0;
+  private cachedRates: Observable<ICurrency> = of();
 
   private api = axios.create({
-    baseURL: this.API_URL,
+    baseURL: API_URL,
   });
 
   getAllRates(): Observable<ICurrency> {
-    return from(this.api.get("/latest")).pipe(
-      map((response: Response) => response.data.rates)
-    );
+    const currentTimestamp = new Date().getTime();
+    const isNewRequest =
+      currentTimestamp - this.lastRequestTimestamp > DIFFERENCE_THRESHOLD_SEC;
+
+    if (isNewRequest) {
+      this.cachedRates = from(this.api.get("/latest")).pipe(
+        map((response: Response) => response.data.rates)
+      );
+      this.lastRequestTimestamp = currentTimestamp;
+    }
+
+    return this.cachedRates;
   }
 
   getAllCurrencies(): Observable<string[]> {
